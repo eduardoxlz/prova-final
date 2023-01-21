af81c9e531317eee5c4d1599bd7bde1f9878c308
eduardoxlz <edupipaskate@gmail.com>
2023-01-20 21:46:44 -0300

    lojainformatica atualizada

--- LojaInformatica/src/main/java/br/com/tech4me/informaticaloja/repository/InformaticaLojaRepositorio.java
+++ LojaInformatica/src/main/java/br/com/tech4me/informaticaloja/repository/InformaticaLojaRepositorio.java
@@ -2,9 +2,9 @@ package br.com.tech4me.informaticaloja.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import br.com.tech4me.informaticaloja.model.Informaticaloja;
+import br.com.tech4me.informaticaloja.model.InformaticaLoja;
 
-public interface InformaticaLojaRepositorio extends MongoRepository<Informaticaloja, String> {
+public interface InformaticaLojaRepositorio extends MongoRepository<InformaticaLoja, String> {
     
 
 
--- LojaInformatica/src/main/java/br/com/tech4me/informaticaloja/service/InformaticaLojaService.java
+++ LojaInformatica/src/main/java/br/com/tech4me/informaticaloja/service/InformaticaLojaService.java
@@ -3,8 +3,8 @@ package br.com.tech4me.informaticaloja.service;
 import java.util.List;
 import java.util.Optional;
 
-import br.com.tech4me.informaticaloja.shared.InformaticaLojaDtoCompleto;
 import br.com.tech4me.informaticaloja.shared.InformaticaLojaDto;
+import br.com.tech4me.informaticaloja.shared.InformaticaLojaDtoCompleto;
 
 public interface InformaticaLojaService {
 
@@ -12,7 +12,7 @@ public interface InformaticaLojaService {
     Optional<InformaticaLojaDto> obterPorId(String id);
     void excluirPorId(String id);
     InformaticaLojaDtoCompleto cadastrar(InformaticaLojaDto dto);
-    Optional<InformaticaLojaCompletoDto> atualizarPorId(String id, InformaticaLojaComopletoDtp dtp);
+    Optional<InformaticaLojaDtoCompleto> atualizarPorId(String id, InformaticaLojaDtoCompleto dto);
     
 
 }
